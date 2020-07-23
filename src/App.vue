a640cf04bd69cd83a5f2ecef08d94f15a4de1956
Simon Pham <simon.pham@pm.me>
2020-07-23 16:37:45 +0200

    infinitescroll

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,21 @@
 <template>
   <v-app :ripple="false">
-    <!-- <logo class="mx-auto" :style="{ width: '30em' }"></logo> -->
-    <v-form ref="form">
-      <v-container
-        @click.ctrl="updateTime()"
-        @click.alt="$vuetify.theme.dark = !$vuetify.theme.dark"
-      >
-        <v-row class="align-center justify-space-around">
-          <edn-load message="edn-load" form="double" anim="dots"></edn-load>
-          <edn-load message="edn-load" form="simple" anim="blink"></edn-load>
-          <edn-load message="edn-load" form="fill"></edn-load>
-        </v-row>
-        <edn-num label="edn-num" v-model="inptNum" tooltip="NOMBRE"></edn-num>
-        <edn-field label="edn-field" v-model="urlString"></edn-field>
-        <edn-url label="edn-url" v-model="urlString" readonly></edn-url>
-        <edn-cat label="edn-cat" v-model="selectedCat" :items="cat"></edn-cat>
-        <edn-cat-x
-          label="edn-cat-x"
-          v-model="ingredient"
-          :items="recette"
-          tooltip="Selection de catégories"
-        ></edn-cat-x>
-        <edn-date v-model="dateInpt" label="edn-date" :popup="true" required />
-        <edn-time
-          v-model="time"
-          label="edn-time"
-          required="Ce champs doit être rempli !"
-        />
-        <v-row class="align-center justify-center">
-          <edn-date
-            v-model="dateInpt"
-            :popup="false"
-            :allowed-dates="allowedDates"
-            class="mx-3"
-          />
-          <edn-time
-            class="mx-3"
-            v-model="time"
-            label="Choisissez une plage horaire"
-            tripStyle
-            :slots="[
-              '00:20',
-              '00:40',
-              '01:20',
-              '01:40',
-              '02:40',
-              '03:40',
-              '04:40',
-              '05:40',
-              '06:40'
-            ]"
-          />
-        </v-row>
-        <edn-memo
-          label="edn-memo"
-          v-model="inptMemo"
-          :size="'normal'"
-          html
-          :required="'Un string'"
-        ></edn-memo>
-        <edn-mail label="edn-mail" v-model="mail" :required="true" />
-        <edn-phone label="edn-phone" v-model="phone"></edn-phone>
-        <edn-check
-          label="edn-check"
-          v-model="checked"
-          :required="true"
-        ></edn-check>
-        <edn-switch label="edn-switch" v-model="switched"></edn-switch>
-        <edn-radio
-          label="edn-radio"
-          :radios="radios"
-          v-model="radioSelect"
-        ></edn-radio>
-        <edn-color label="edn-color" v-model="selectedColor"></edn-color>
-        <v-row class="justify-center align-self-center">
-          <edn-btn class="mx-2" validation="skin2019" @click="Validate()"
-            >Valider</edn-btn
-          >
-          <edn-btn class="mx-2" validation @click="Validate()">Valider</edn-btn>
-          <edn-btn class="mx-2" @click="Reset()">Reset</edn-btn>
-          <edn-btn class="mx-2" color="secondary" @click="Reset()"
-            >Reset</edn-btn
-          >
-        </v-row>
-        <edn-list-mod
-          ref="infScroll"
-          :headers="headers"
-          :listMaxLength="listMaxLength"
-          :nbItemToAdd="nbItemToAdd"
-          v-if="users"
-          :items="users"
-        ></edn-list-mod>
-      </v-container>
-    </v-form>
+    <edn-list-mod
+      ref="infScroll"
+      :headers="headers"
+      :listMaxLength="listMaxLength"
+      :nbItemToAdd="nbItemToAdd"
+      v-if="users"
+      :items="users"
+      :page="1"
+      :monevent="toto"
+    ></edn-list-mod>
   </v-app>
 </template>
 
 <script>
+import apiModule from "@/modules/api.js";
+
 export default {
   name: "App",
 
@@ -110,7 +29,7 @@ export default {
       mail: "",
       checked: true,
       switched: true,
-      allowedDates: val => parseInt(val.split("-")[2], 10) % 2 === 0,
+      allowedDates: (val) => parseInt(val.split("-")[2], 10) % 2 === 0,
       inptText: "efef",
       dateInpt: "",
       inptMemo: "",
@@ -123,12 +42,12 @@ export default {
       radios: [
         {
           label: "Choix 1",
-          value: "1"
+          value: "1",
         },
         {
           label: "Choix 2",
-          value: "2"
-        }
+          value: "2",
+        },
       ],
       contentAbc: "abcdefghijklmnopqtrsuvwxyz",
       date: "",
@@ -138,7 +57,7 @@ export default {
       urlErrorUpdate: false,
 
       //Datas du mode liste
-      users: require("./components/assets/localDatas.json"),
+      users: [],
       nbItemToAdd: 3,
       listMaxLength: 20,
       infinityList: [],
@@ -146,21 +65,46 @@ export default {
       scrollTime: 0,
       //Header du mode liste
       headers: [
-        {
-          text: "UserHotcom",
-          align: "start",
-          sortable: false,
-          value: "UserHotcom.Login"
-        },
-        { text: "Portable", value: "Portable" },
         { text: "Mail", value: "Email" },
         { text: "Prénom", value: "FirstName" },
         { text: "Nom", value: "LastName" },
-        { text: "Trigramme", value: "Trigramme" }
-      ]
+      ],
     };
   },
   methods: {
+    async toto(mapage) {
+      if (mapage < 0) mapage = 0;
+
+      let myApi = new apiModule(
+        "https://ww2.eudonet.com/eudoapi",
+        "asH1XaS/nVQmqREI4UyYjkZIatlAIEpvWe9gMsXPPH2qyWT3TRss4UMvxWcSYFkT6NYATttwNaFrmJmKjrQU8C3/ts2QFdjEwGZEmkGRtl6OKIyEIAPk4rNi8rhpF/7o6Keg1wukC4EIw1bFM2Kk4iRgWV6pnQ0hPHBzqQleXm7biu1fJnsDylIT2AQo6prj"
+      );
+
+      let resp = await myApi.APIGetList(
+        200,
+        [201, 202, 212, 275, 237],
+        mapage,
+        5867
+      );
+
+      resp.Rows.forEach((element) => {
+        var a = element.Fields[0].Value;
+        var b = element.Fields[1].Value;
+        var c = element.Fields[2].Value;
+
+        var myPP = {
+          LastName: a,
+          FirstName: b,
+          Email: c,
+        };
+
+        this.users.push(myPP);
+        if (this.users.length > 40) {
+          this.users.shift(0);
+        }
+      });
+    },
+
     updateTime() {
       this.time = "09:00";
     },
@@ -172,7 +116,7 @@ export default {
     },
     testUrl(event) {
       console.log(event);
-    }
-  }
+    },
+  },
 };
 </script>
--- src/components/ednListMod.vue
+++ src/components/ednListMod.vue
@@ -1,11 +1,7 @@
 <template>
   <v-row colspan="12" :ripple="false">
     <v-col cols="2" v-show="false">
-      <v-list
-        dense
-        height="25em"
-        :style="showAllFilters ? 'overflow:overlay' : 'overflow:hidden'"
-      >
+      <v-list dense height="25em" :style="showAllFilters ? 'overflow:overlay' : 'overflow:hidden'">
         <v-list-item
           v-show="
             !infinityList.filter(itm => itm.UserHotcom.Group.Name == item.Name)
@@ -23,12 +19,12 @@
             :color="selectedFilter == item.Id ? 'primary' : ''"
             text
           >
-            <span> {{ item.Name }} </span>
+            <span>{{ item.Name }}</span>
             <span>
               {{
-                (item.Number = infinityList.filter(
-                  itm => itm.UserHotcom.Group.Name == item.Name
-                ).length)
+              (item.Number = infinityList.filter(
+              itm => itm.UserHotcom.Group.Name == item.Name
+              ).length)
               }}
             </span>
           </v-btn>
@@ -40,8 +36,7 @@
         class="d-block mx-auto my-2"
         x-small
         @click="showAllFilters = !showAllFilters"
-        >Afficher tout les résultats</v-btn
-      >
+      >Afficher tout les résultats</v-btn>
     </v-col>
     <v-col cols="12">
       <v-data-table
@@ -58,8 +53,7 @@
         disable-pagination
         hide-default-footer
         :height="height"
-      >
-      </v-data-table>
+      ></v-data-table>
     </v-col>
   </v-row>
 </template>
@@ -69,23 +63,38 @@ export default {
   props: {
     headers: {
       type: Array,
-      default: () => []
+      default: () => [],
     },
+
+    monevent: {
+      type: Function,
+      default: () => {
+        () => "toto";
+      },
+    },
+
+    page: {
+      type: Number,
+      default: () => {
+        () => 1;
+      },
+    },
+
     items: {
       type: Array,
-      default: () => []
+      default: () => [],
     },
     nbItemToAdd: {
       type: Number,
-      default: () => 3
+      default: () => 3,
     },
     listMaxLength: {
       type: Number,
-      default: () => 20
+      default: () => 20,
     },
     height: {
       type: Number,
-      default: () => 500
+      default: () => 500,
     },
 
     groupHeader: {
@@ -95,31 +104,39 @@ export default {
           text: "",
           align: "start",
           sortable: false,
-          value: "Name"
+          value: "Name",
         },
-        { text: "", value: "Portable" }
-      ]
-    }
+        { text: "", value: "Portable" },
+      ],
+    },
   },
   data() {
     return {
       scrollTime: 0,
       currentOrder: 0,
+      currentPage: 1,
 
-      //Gestion de la liste
-      infinityList: [],
+      //Gestion de la liste      
       infinityListFltrd: [],
 
       //Gestion des filtres
       itemsFilters: [],
       selectedFilter: null,
       itemSelected: [],
-      showAllFilters: false
+      showAllFilters: false,
     };
   },
+
+
+computed : {
+
+    infinityList : function(){
+      return this.items;
+    }
+}  ,
   methods: {
-    getItems(e) {
-      if (!e) e = WheelEvent;
+    async getItems(e) {
+       
 
       let direction;
       let position = this.$refs["edn-scroll"].$el.children[0].scrollTop;
@@ -129,24 +146,22 @@ export default {
       if (e.wheelDeltaY > 0) direction = "up";
       if (e.wheelDeltaY < 0) direction = "down";
 
-      if (direction != "down" && direction != "up") {
-        if (this.currentOrder < this.infinityList.length + this.listMaxLength) {
-          this.infinityList = this.infinityList.concat(
-            this.items.slice(
-              this.infinityList.length,
-              this.infinityList.length + this.listMaxLength
-            )
-          );
-
-          this.currentOrder = this.listMaxLength;
-        }
-      }
+ 
+ console.log("getitems")
+      if (direction == "down") 
+        this.currentPage = this.currentPage + 1;
+      else if (direction == "up")
+         this.currentPage = this.currentPage - 1;
+ 
+      await this.monevent(this.currentPage);
+/*
+      this.infinityList = this.users;
 
       //WHEEL UP
       if (this.currentOrder > this.listMaxLength && direction == "up") {
         this.infinityList.unshift(
           this.items.filter(
-            item =>
+            (item) =>
               item.order >=
               this.currentOrder - this.listMaxLength - this.nbItemToAdd
           )[0]
@@ -160,39 +175,45 @@ export default {
         this.infinityList.shift();
 
         this.infinityList.push(
-          this.items.filter(item => item.order >= this.currentOrder)[0]
+          this.items.filter((item) => item.order >= this.currentOrder)[0]
         );
 
         this.currentOrder = this.currentOrder + this.nbItemToAdd;
       }
+*/
       setTimeout(() => {
         this.itemsFilters.sort((a, b) => b.Number - a.Number), 300;
       });
     },
+
     addFltr(id) {
       if (this.selectedFilter != id) {
         this.selectedFilter = id;
         this.infinityListFltrd = this.infinityList.filter(
-          itm => itm.UserHotcom.Group.Id == id
+          (itm) => itm.UserHotcom.Group.Id == id
         );
       } else {
         this.selectedFilter = null;
       }
-    }
+    },
   },
-  mounted() {
+
+  async mounted() {
     if (this.items.length != 0)
       this.items.forEach((item, index) => {
         if (
-          !this.itemsFilters.filter(itm => itm.Id == item.UserHotcom.Group.Id)
+          !this.itemsFilters.filter((itm) => itm.Id == item.UserHotcom.Group.Id)
             .length != 0
         ) {
           this.itemsFilters.push(item.UserHotcom.Group);
         }
         item.order = index;
       });
-    this.getItems();
-  }
+
+    this.currentPage = 0;
+
+    await this.getItems(this.page);
+  },
 };
 </script>
 
